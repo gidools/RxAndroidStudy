e00ed8ab7255e5048b138d69a2156e9d3ccaa56b
Jack <gskwon@maxst.com>
2016-10-28 22:12:28 +0900

    lamda 식 신기하군..

--- app/src/main/java/maxst/com/rxandroidstudy/fragment/CombinedLatestFragment.java
+++ app/src/main/java/maxst/com/rxandroidstudy/fragment/CombinedLatestFragment.java
@@ -3,6 +3,7 @@ package maxst.com.rxandroidstudy.fragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -43,14 +44,11 @@ public class CombinedLatestFragment extends Fragment {
 		EditText editText1 = (EditText)layout.findViewById(R.id.editText1);
 
 		Observable<Boolean> checks1 = RxCompoundButton.checkedChanges(checkBox1);
-//		checks1.subscribe(check -> {
-//			Log.i(TAG, "Check changed : " + check);
-//			editText1.setEnabled(check);
-//		});
+		checks1.subscribe(editText1::setEnabled);
 
 		Observable<Boolean> textExist1 = RxTextView.textChanges(editText1)
 				.map(TextUtils::isEmpty)
-				.map(empty -> {return !empty;});
+				.map(empty -> !empty);
 
 		Observable<Boolean> textValidations1 = Observable.combineLatest(checks1, textExist1, (check, exist) -> !check || exist);
 
@@ -58,11 +56,11 @@ public class CombinedLatestFragment extends Fragment {
 		EditText editText2 = (EditText) layout.findViewById(R.id.editText2);
 
 		Observable<Boolean> checks2 = RxCompoundButton.checkedChanges(checkBox2);
-//		checks2.subscribe(check -> editText2.setEnabled(check));
+		checks2.subscribe(editText2::setEnabled);
 
 		Observable<Boolean> textExist2 = RxTextView.textChanges(editText2)
 				.map(TextUtils::isEmpty)
-				.map(empty -> {return !empty;});
+				.map(empty -> !empty);
 
 		Observable<Boolean> textValidations2 = Observable.combineLatest(checks2, textExist2, (check, exist) -> !check || exist);
 
@@ -70,20 +68,20 @@ public class CombinedLatestFragment extends Fragment {
 		EditText editText3 = (EditText) layout.findViewById(R.id.editText3);
 
 		Observable<Boolean> checks3 = RxCompoundButton.checkedChanges(checkBox3);
-//		checks3.subscribe(check -> editText3.setEnabled(check));
+		checks3.subscribe(editText3::setEnabled);
 
 		Observable<Boolean> textExist3 = RxTextView.textChanges(editText3)
 				.map(TextUtils::isEmpty)
-				.map(empty -> {return !empty;});
+				.map(empty -> !empty);
 
 		Observable<Boolean> textValidations3 = Observable.combineLatest(checks3, textExist3, (check, exist) -> !check || exist);
 
 		Button loginBtn = (Button)layout.findViewById(R.id.loginBtn);
 
-		Observable.combineLatest(textValidations1, textValidations2, textValidations3,
-				(validation1, validation2, validation3) ->
-						validation1 && validation2 && validation3)
-				.subscribe(validation -> loginBtn.setEnabled(validation));
+//		Observable.combineLatest(textValidations1, textValidations2, textValidations3,
+//				(validation1, validation2, validation3) ->
+//						validation1 && validation2 && validation3)
+//				.subscribe(loginBtn::setEnabled);
 
 		return layout;
 	}
