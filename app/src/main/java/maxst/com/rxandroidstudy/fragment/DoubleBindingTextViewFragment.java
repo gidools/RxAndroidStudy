2c1fd97c7388315eb9c69e534052a43f0347ac8e
Jack <gskwon@maxst.com>
2016-10-30 22:26:41 +0900

    첫 번째 EditText 에 숫자 입력 된 것 없을 때 계산 올바르게 되는 문제 수정

--- app/src/main/java/maxst/com/rxandroidstudy/fragment/DoubleBindingTextViewFragment.java
+++ app/src/main/java/maxst/com/rxandroidstudy/fragment/DoubleBindingTextViewFragment.java
@@ -19,6 +19,7 @@ import maxst.com.rxandroidstudy.R;
 import rx.Observable;
 import rx.Subscription;
 import rx.subjects.PublishSubject;
+
 /**
  * Created by Giseok on 2016-10-30.
  */
@@ -43,18 +44,46 @@ public class DoubleBindingTextViewFragment extends Fragment {
 		View layout = inflater.inflate(R.layout.fragment_double_binding_textview, container, false);
 		ButterKnife.bind(this, layout);
 
-		rx.Observable<TextViewTextChangeEvent> numb1Observable = RxTextView.textChangeEvents(num1EditText)
-				.filter(textChanges -> !TextUtils.isEmpty(textChanges.text().toString()));
+		// region -- bug code
+//		rx.Observable<TextViewTextChangeEvent> numb1Observable = RxTextView.textChangeEvents(num1EditText)
+//				.filter(textChanges -> !TextUtils.isEmpty(textChanges.text().toString()));
+//
+//		rx.Observable<TextViewTextChangeEvent> numb2Observable = RxTextView.textChangeEvents(num2EditText)
+//				.filter(textChanges -> !TextUtils.isEmpty(textChanges.text().toString()));
+//
+//		Observable.combineLatest(numb1Observable, numb2Observable,
+//				(textChangeEvents1, textChangeEvents2) -> {
+//					float num1 = Float.parseFloat(textChangeEvents1.text().toString());
+//					float num2 = Float.parseFloat(textChangeEvents2.text().toString());
+//					return num1 + num2; }) //)
+//				.subscribe(result -> resultTextView.setText(String.valueOf(result)));
+		// endregion -- bug code
 
-		rx.Observable<TextViewTextChangeEvent> numb2Observable = RxTextView.textChangeEvents(num2EditText)
-				.filter(textChanges -> !TextUtils.isEmpty(textChanges.text().toString()));
+		// region -- bug fix
+		rx.Observable<Float> numb1Observable = RxTextView.textChangeEvents(num1EditText)
+				.map(textChanges -> {
+					float num = 0;
+					if (!TextUtils.isEmpty(textChanges.text().toString())) {
+						num = Float.parseFloat(textChanges.text().toString());
+					}
+					return num;
+				});
+
+		rx.Observable<Float> numb2Observable = RxTextView.textChangeEvents(num2EditText)
+				.map(textChanges -> {
+					float num = 0;
+					if (!TextUtils.isEmpty(textChanges.text().toString())) {
+						num = Float.parseFloat(textChanges.text().toString());
+					}
+					return num;
+				});
 
 		Observable.combineLatest(numb1Observable, numb2Observable,
-				(textChangeEvents1, textChangeEvents2) -> {
-					float num1 = Float.parseFloat(textChangeEvents1.text().toString());
-					float num2 = Float.parseFloat(textChangeEvents2.text().toString());
-					return num1 + num2; }) //)
+				(num1, num2) -> {
+					return num1 + num2;
+				})
 				.subscribe(result -> resultTextView.setText(String.valueOf(result)));
+		// endregion -- bug fix
 
 		return layout;
 	}
