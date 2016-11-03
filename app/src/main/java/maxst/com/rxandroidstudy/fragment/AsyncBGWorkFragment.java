876ce23394be060dded4a19e59b2488735536301
gskwon <gskwon@maxst.com>
2016-11-03 18:05:31 +0900

    Async callback 쓰는 방법도 추가

--- app/src/main/java/maxst/com/rxandroidstudy/fragment/AsyncBGWorkFragment.java
+++ app/src/main/java/maxst/com/rxandroidstudy/fragment/AsyncBGWorkFragment.java
@@ -87,13 +87,13 @@ public class AsyncBGWorkFragment extends Fragment {
 				addLog("Within runnable");
 				String result = doSomeLongOperationThatBlocksCurrentThread();
 
-				getActivity().runOnUiThread(new Runnable() {
-					@Override
-					public void run() {
-						addLog(result);
-						progressBar.setVisibility(View.INVISIBLE);
-					}
-				});
+//				getActivity().runOnUiThread(new Runnable() {
+//					@Override
+//					public void run() {
+//						addLog(result);
+//						progressBar.setVisibility(View.INVISIBLE);
+//					}
+//				});
 			}
 		});
 	}
@@ -131,15 +131,18 @@ public class AsyncBGWorkFragment extends Fragment {
 			Thread.sleep(3000);
 		} catch (InterruptedException e) {
 			Log.d(TAG, "Operation was interrupted");
+			workerCallback.onError(e);
 
-			getActivity().runOnUiThread(new Runnable() {
-				@Override
-				public void run() {
-					addLog("Error occurred!!");
-					progressBar.setVisibility(View.INVISIBLE);
-				}
-			});
+//			getActivity().runOnUiThread(new Runnable() {
+//				@Override
+//				public void run() {
+//					addLog("Error occurred!!");
+//					progressBar.setVisibility(View.INVISIBLE);
+//				}
+//			});
 		}
+
+		workerCallback.onComplete("BG work completed");
 
 		return "BG work completed";
 	}
@@ -151,4 +154,35 @@ public class AsyncBGWorkFragment extends Fragment {
 			super(context, R.layout.item_log, R.id.item_log, logs);
 		}
 	}
+
+	private interface WorkerCallback {
+		void onError(Exception e);
+
+		void onComplete(String result);
+	}
+
+	private WorkerCallback workerCallback = new WorkerCallback() {
+
+		@Override
+		public void onError(Exception e) {
+			getActivity().runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					addLog(e.getMessage());
+					progressBar.setVisibility(View.INVISIBLE);
+				}
+			});
+		}
+
+		@Override
+		public void onComplete(String result) {
+			getActivity().runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					addLog(result);
+					progressBar.setVisibility(View.INVISIBLE);
+				}
+			});
+		}
+	};
 }
