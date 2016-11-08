8f09f47b2589993f60f825d0ded0aa8501cfe179
gskwon <gskwon@maxst.com>
2016-11-08 16:30:55 +0900

    1. ButterKnife 라이브러리 방식으로 코딩 2. 쓸데없는 테스트 코드들 삭제

---
+++ app/src/main/java/maxst/com/rxandroidstudy/fragment/FlatMapHttpFragment.java
@@ -0,0 +1,169 @@
+package maxst.com.rxandroidstudy.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import maxst.com.rxandroidstudy.R;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+import rx.subscriptions.CompositeSubscription;
+
+public class FlatMapHttpFragment extends Fragment {
+
+	private static final String TAG = FlatMapHttpFragment.class.getSimpleName();
+	private CompositeSubscription compositeSubscription;
+
+	@Bind(R.id.list_exhibition)
+	ListView exhibitionListView;
+
+	@Bind(R.id.btn_start_operation)
+	Button startBtn;
+
+	private ExhibitionAdapter exhibitionAdapter;
+	private List<Exhibition> exbiExhibitionList;
+
+	public FlatMapHttpFragment() {
+		// Required empty public constructor
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+							 Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		View layout = inflater.inflate(R.layout.fragment_flat_map_http, container, false);
+		ButterKnife.bind(this, layout);
+
+		exhibitionAdapter = new ExhibitionAdapter(getContext(), R.layout.item_exhibition, new ArrayList<>());
+		exhibitionListView.setAdapter(exhibitionAdapter);
+		compositeSubscription = new CompositeSubscription();
+		return layout;
+	}
+
+	/**
+	 * Json parsing is recommended another thread from UI thread
+	 */
+	@OnClick(R.id.btn_start_operation)
+	public void startOperation() {
+		exhibitionAdapter.clear();
+
+		compositeSubscription.add(
+				requestExhibitionObservable("http://api.arcube.co.kr/api/exhibitions?ordertype=popular")
+//				requestExhibitionObservable("http://api.arcube.co.k/api/exhibitions?ordertype=popular") // Make error intentionally
+						.flatMap(response -> {
+							Type collectionType = new TypeToken<List<Exhibition>>() { }.getType();
+							Gson gson = new GsonBuilder().create();
+							List<Exhibition> exhibitions = gson.fromJson(response, collectionType);
+							Log.d(TAG, "Current thread is main thread : " + (Looper.myLooper() == Looper.getMainLooper()));
+							return Observable.from(exhibitions);
+						})
+						.subscribeOn(Schedulers.io())
+						.observeOn(AndroidSchedulers.mainThread())
+						.subscribe(exhibition -> exhibitionAdapter.add(exhibition)
+								, error -> Toast.makeText(getContext(), "Error : " + error.getMessage(), Toast.LENGTH_SHORT).show()
+								, () -> Toast.makeText(getContext(), "onCompleted", Toast.LENGTH_SHORT).show()
+						)
+		);
+	}
+
+	private Observable<String> requestExhibitionObservable(String url) {
+		return Observable.create(new Observable.OnSubscribe<String>() {
+
+			@Override
+			public void call(Subscriber<? super String> observer) {
+				try {
+					HttpURLConnection connection =
+							(HttpURLConnection) new URL(url).openConnection();
+
+					connection.setRequestMethod("GET");
+					connection.setUseCaches(false);
+					connection.setDoInput(true);
+					connection.setConnectTimeout(10 * 1000);
+					connection.setReadTimeout(10 * 1000);
+					InputStream responseStream = connection.getInputStream();
+					String response = drainStream(responseStream);
+					responseStream.close();
+					connection.disconnect();
+					observer.onNext(response);
+				} catch (IOException e) {
+					observer.onError(e);
+				} finally {
+					observer.onCompleted();
+				}
+			}
+		});
+	}
+
+	private static String drainStream(InputStream in) {
+		Scanner s = new Scanner(in).useDelimiter("\\A");
+		return s.hasNext() ? s.next() : "";
+	}
+
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+
+		compositeSubscription.unsubscribe();
+		ButterKnife.unbind(this);
+	}
+
+private static class Exhibition {
+	public int id;
+	public String name;
+	public String price;
+}
+
+private static class ExhibitionAdapter extends ArrayAdapter<Exhibition> {
+
+	public ExhibitionAdapter(Context context, int resource, ArrayList<Exhibition> exhibitions) {
+		super(context, resource, exhibitions);
+	}
+
+	@NonNull
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		if (convertView == null) {
+			convertView = View.inflate(getContext(), R.layout.item_exhibition, null);
+		}
+
+		TextView id = (TextView) convertView.findViewById(R.id.item_id);
+		TextView name = (TextView) convertView.findViewById(R.id.item_name);
+		TextView price = (TextView) convertView.findViewById(R.id.item_price);
+
+		Exhibition exhibition = getItem(position);
+		id.setText(String.valueOf(exhibition.id));
+		name.setText(exhibition.name);
+		price.setText(exhibition.price);
+		return convertView;
+	}
+}
+}
--- app/src/main/java/maxst/com/rxandroidstudy/fragment/MainFragment.java
+++ app/src/main/java/maxst/com/rxandroidstudy/fragment/MainFragment.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import maxst.com.rxandroidstudy.R;
 
 public class MainFragment extends Fragment {
@@ -26,108 +28,103 @@ public class MainFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		View layout =  inflater.inflate(R.layout.fragment_main, container, false);
-		layout.findViewById(R.id.rx_basic).setOnClickListener(onClickListener);
-		layout.findViewById(R.id.rx_lambda).setOnClickListener(onClickListener);
-		layout.findViewById(R.id.rx_map).setOnClickListener(onClickListener);
-		layout.findViewById(R.id.rx_view_click).setOnClickListener(onClickListener);
-		layout.findViewById(R.id.rx_merge).setOnClickListener(onClickListener);
-		layout.findViewById(R.id.rx_scan).setOnClickListener(onClickListener);
-		layout.findViewById(R.id.rx_scan2).setOnClickListener(onClickListener);
-		layout.findViewById(R.id.rx_combined_latest).setOnClickListener(onClickListener);
-		layout.findViewById(R.id.rx_combined_latest).setOnClickListener(onClickListener);
-		layout.findViewById(R.id.flat_map).setOnClickListener(onClickListener);
-		layout.findViewById(R.id.rx_map_vs_flat_map).setOnClickListener(onClickListener);
-		layout.findViewById(R.id.async_bg_work).setOnClickListener(onClickListener);
-		layout.findViewById(R.id.scheduler_bg_work).setOnClickListener(onClickListener);
-		layout.findViewById(R.id.debounce_emitter).setOnClickListener(onClickListener);
-		layout.findViewById(R.id.double_binding_text_view).setOnClickListener(onClickListener);
-		layout.findViewById(R.id.buffer_demo).setOnClickListener(onClickListener);
-		layout.findViewById(R.id.form_validation).setOnClickListener(onClickListener);
-		layout.findViewById(R.id.polling).setOnClickListener(onClickListener);
-		layout.findViewById(R.id.display_bitmap).setOnClickListener(onClickListener);
+		View layout = inflater.inflate(R.layout.fragment_main, container, false);
+		ButterKnife.bind(this, layout);
+
 		return layout;
 	}
 
-	private View.OnClickListener onClickListener = new View.OnClickListener() {
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
 
-		@Override
-		public void onClick(View v) {
-			switch(v.getId()) {
-				case R.id.rx_basic:
-					clickedOn(new ObservableAndSubscriberFragment());
-					break;
+		ButterKnife.unbind(this);
+	}
 
-				case R.id.rx_lambda:
-					clickedOn(new LambdaFragment());
-					break;
+	@OnClick(R.id.rx_basic)
+	public void startBasic() {
+		clickedOn(new ObservableAndSubscriberFragment());
+	}
 
-				case R.id.rx_map:
-					clickedOn(new MapFragment());
-					break;
+	@OnClick(R.id.rx_lambda)
+	public void lambdaExample() {
+		clickedOn(new LambdaFragment());
+	}
 
-				case R.id.rx_view_click:
-					clickedOn(new ViewClickFragment());
-					break;
+	@OnClick(R.id.rx_map)
+	public void mapExample() {
+		clickedOn(new MapFragment());
+	}
 
-				case R.id.rx_merge:
-					clickedOn(new MergeFragment());
-					break;
+	@OnClick(R.id.rx_view_click)
+	public void viewClickExample() {
+		clickedOn(new ViewClickFragment());
+	}
 
-				case R.id.rx_scan:
-					clickedOn(new ScanFragment());
-					break;
+	@OnClick(R.id.rx_merge)
+	public void merge() {
+		clickedOn(new MergeFragment());
+	}
 
-				case R.id.rx_scan2:
-					clickedOn(new Scan2Fragment());
-					break;
+	@OnClick(R.id.rx_scan)
+	public void scan() {
+		clickedOn(new ScanFragment());
+	}
 
-				case R.id.rx_combined_latest:
-					clickedOn(new CombinedLatestFragment());
-					break;
+	@OnClick(R.id.rx_combined_latest)
+	public void combinedLatest() {
+		clickedOn(new CombinedLatestFragment());
+	}
 
-				case R.id.rx_map_vs_flat_map:
-					clickedOn(new MapVsFlatMapFragment());
-					break;
+	@OnClick(R.id.rx_flat_map)
+	public void mapVsFlatMap() {
+		clickedOn(new FlatMapFragment());
+	}
 
-				case R.id.flat_map:
-					clickedOn(new FlatMapFragment());
-					break;
+	@OnClick(R.id.flat_map_http)
+	public void flatMap() {
+		clickedOn(new FlatMapHttpFragment());
+	}
 
-				case R.id.async_bg_work:
-					clickedOn(new AsyncBGWorkFragment());
-					break;
+	@OnClick(R.id.async_bg_work)
+	public void asyncBgWork() {
+		clickedOn(new AsyncBGWorkFragment());
+	}
 
-				case R.id.scheduler_bg_work:
-					clickedOn(new SchedulerBGWorkFragment());
-					break;
+	@OnClick(R.id.scheduler_bg_work)
+	public void scheduler() {
+		clickedOn(new SchedulerBGWorkFragment());
+	}
 
-				case R.id.debounce_emitter:
-					clickedOn(new DebounceSearchEmitterFragment());
-					break;
+	@OnClick(R.id.debounce_emitter)
+	public void debounce() {
+		clickedOn(new DebounceSearchEmitterFragment());
+	}
 
-				case R.id.double_binding_text_view:
-					clickedOn(new DoubleBindingTextViewFragment());
-					break;
+	@OnClick(R.id.double_binding_text_view)
+	public void doubleBinding() {
+		clickedOn(new DoubleBindingTextViewFragment());
+	}
 
-				case R.id.buffer_demo:
-					clickedOn(new BufferDemoFragment());
-					break;
+	@OnClick(R.id.buffer_demo)
+	public void buffer() {
+		clickedOn(new BufferDemoFragment());
+	}
 
-				case R.id.form_validation:
-					clickedOn(new FormValidationFragment());
-					break;
+	@OnClick(R.id.form_validation)
+	public void formValidation() {
+		clickedOn(new FormValidationFragment());
+	}
 
-				case R.id.polling:
-					clickedOn(new PollingFragment());
-					break;
+	@OnClick(R.id.polling)
+	public void polling() {
+		clickedOn(new PollingFragment());
+	}
 
-				case R.id.display_bitmap:
-					clickedOn(new DisplayBitmapFragment());
-					break;
-			}
-		}
-	};
+	@OnClick(R.id.display_bitmap)
+	public void displayBitmap() {
+		clickedOn(new DisplayBitmapFragment());
+	}
 
 	private void clickedOn(@NonNull Fragment fragment) {
 		final String tag = fragment.getClass().toString();
